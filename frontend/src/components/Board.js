9b2397a24ebd363a2d8d85a8c07e365ea0a73c7d
Alejandro Sánchez Yalí <alejandro.driveyali@gmail.com>
2021-03-20 18:38:52 -0500

    feat: board game.

---
+++ frontend/src/components/Board.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Square from './Square'
+
+
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '350px',
+    height: '350px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(7, 1fr) / repeat(7, 1fr)'
+}
+
+const Board = ({ squares, onClick }) => {
+
+    return (
+    <div style={style}> 
+        {squares.map((square, i) => (
+            <Square key={i} value={square} onClick={() => onClick(i)}/>
+        ))}
+    </div>
+    )
+}
+
+export default Board
